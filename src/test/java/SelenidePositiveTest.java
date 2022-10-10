2870eb5ea2101997257c0451025ccd751936a2b1
Людмила Трибушинина <shilova_lu@mail.ru>
2022-10-10 21:20:34 +0400

    Created Positive test

---
+++ src/test/java/SelenidePositiveTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenidePositiveTest {
+    String generateDate(int days){
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    @Test
+    void shouldBeSuccess() {
+        String date = generateDate(3);
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Москва");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT,Keys.HOME),Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue("14.10.2022");
+        $("[data-test-id='name'] input").setValue("Толстой Лев");
+        $("[data-test-id='phone'] input").setValue("+79999999999");
+        $("[data-test-id='agreement']").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $(withText("Встреча успешно забронирована на")).shouldBe(visible, Duration.ofSeconds(12));
+
+    }
+
+}
